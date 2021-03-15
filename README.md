6f003565d080f2599f92f7ff0571aad2f291a4eb
Jimbles <djamesavery@gmail.com>
2021-03-15 14:40:51 +0000

    Button listener code and part list

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PiNAS
-Raspberry Pi Torrent Box, NAS and Moonlight client. VESA mounted
+Raspberry Pi Torrent Box, NAS and Moonlight client. VESA mounted on the back of the Telly with external HDD
+
+Parts used:
+- Raspberry Pi 4GB
+- WD Elements 2TB HDD WDBU6Y0020BBK
+- 40mm fan and heatsink (I used [this one](https://www.ebay.co.uk/itm/Raspberry-Pi-Fan-Heatsink-Cooling-Kit-rubber-silicone-feet-40mm-5V-Pi4-4b-3-3b/133433997199?ssPageName=STRK%3AMEBIDX%3AIT&_trksid=p2057872.m2749.l2649) )
+- Push buttons and an LED
+- 3D print case, lids and button holder
+
+### Case and Buttons
+
+### Media Setup
+
+### Torrents
+
+### Streaming
---
+++ code/moonlight_shutdown.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env/python3
+
+# Script for shutdown and moonlight buttons.
+# Add to rc.local for startup
+# https://core-electronics.com.au/tutorials/how-to-make-a-safe-shutdown-button-for-raspberry-pi.html
+
+
+
+from gpiozero import Button, LED
+from subprocess import check_call
+from signal import pause
+from time import sleep
+from os import system 
+
+led = LED(18)
+
+def moonlight():
+    print('Starting Moonlight')
+    print('Killing running moonlight-qt processes...')
+    try:        
+        check_call(['killall', 'moonlight-qt']) # this will error if none running so will avoid sleep below
+        #os.system("killall moonlight-qt")
+        sleep(2) # wait a bit for it to close propery (not sure if needed)
+    except:
+        print('Moonlight not running')
+
+    system("QT_QPA_EGLFS_ALWAYS_SET_MODE=1 moonlight-qt &") # this way it run in background
+    print("Done")
+
+def shutdown():
+    print("Shutting Down")
+    led.on() #led will turn off after shutdown so know its happened
+    sleep(1)
+    check_call(['sudo', 'poweroff'])
+    #system('shutdown now -h')
+
+moonlight_btn = Button(14, hold_time=2)
+moonlight_btn.when_held = moonlight
+
+shutdown_btn = Button(15, hold_time=2)
+shutdown_btn.when_held = shutdown
+
+print("Starting Button Listener")
+pause()
+
